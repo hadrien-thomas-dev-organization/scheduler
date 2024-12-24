3be3df66f33d645283bd41945b3912c2349a96ef
Hadrien Thomas <hadrien.thomas.dev@gmail.com>
2024-12-24 10:23:48 +0100

    [chore] vite to parcel + first calendar

---
+++ src/components/Calendar.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { DayPilotMonth } from '@daypilot/daypilot-lite-vue';
+import { format } from 'date-fns';
+import { ref } from 'vue';
+
+const startDate = ref(format(new Date(), 'yyyy-MM-dd'));
+
+</script>
+
+<template>
+    <h1>Calendar for this month</h1>
+    <DayPilotMonth :startDate="startDate" />
+</template>
