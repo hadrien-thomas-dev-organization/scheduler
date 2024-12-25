f56c8f8673a07773fa41a8655258114f8cacd179
Hadrien Thomas <hadrien.thomas.dev@gmail.com>
2024-12-25 21:37:13 +0100

    [chore] repair workflow and show version on app

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <script setup lang="ts">
 import Calendar from './components/Calendar.vue';
+import packageJson from '../package.json'; // Ajustez le chemin selon votre structure
+const appVersion = packageJson.version;
+
 </script>
 
 <template>
   <Calendar />
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p>
+        <strong>Scheduler</strong> version {{ appVersion }}
+      </p>
+    </div>
+  </footer>
 </template>
